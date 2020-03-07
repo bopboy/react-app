408cd531ee0f8f978cd454d4efdc29b7f1945938
bopboy2 <scp0000@gmail.com>
2020-03-08 08:56:37 +0900

    17

--- src/App.js
+++ src/App.js
@@ -6,10 +6,21 @@ import './App.css'
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: {
+        title: "WEB", sub: "World Wide Web!!"
+      }
+    }
+  }
   render() {
     return (
       <div className="App" >
-        <Subject title="WEB" sub="World Wide Web!"></Subject>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}>
+        </Subject>
         <TOC></TOC>
         <Contents title="HTML" desc="HTML is HyperText Markup Language"></Contents>
       </div>
