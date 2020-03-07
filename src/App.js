87dca634a3a2df9be157bf892d3319a317a86591
bopboy2 <scp0000@gmail.com>
2020-03-07 17:55:16 +0900

    11

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 import './App.css'
+
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+        <h1>WEB</h1>
+        world wide web!
+      </header>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App" >
-        Hello React!!
+        <Subject></Subject>
       </div>
     );
   }
