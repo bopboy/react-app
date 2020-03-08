172b7761364433c2973b946c80a199655a7ad949
bopboy2 <scp0000@gmail.com>
2020-03-09 08:22:46 +0900

    19

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      mode: "welcome",
       subject: { title: "WEB", sub: "World Wide Web!!" },
+      welcome: { tilte: "Welcome", desc: "Welcome, React!!" },
       contents: [
         { id: 1, title: 'HTML', desc: 'HTML is for information' },
         { id: 2, title: 'CSS', desc: 'CSS is for design' },
@@ -18,6 +20,14 @@ class App extends Component {
     }
   }
   render() {
+    var _title, _desc = null;
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App" >
         <Subject
@@ -25,10 +35,7 @@ class App extends Component {
           sub={this.state.subject.sub}>
         </Subject>
         <TOC data={this.state.contents}></TOC>
-        <Contents
-          title="HTML"
-          desc="HTML is HyperText Markup Language">
-        </Contents>
+        <Contents title={_title} desc={_desc}></Contents>
       </div>
     );
   }
--- src/components/Contents.js
+++ src/components/Contents.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 class Contents extends Component {
     render() {
+        console.log("Contents render")
         return (
             <article>
                 <h2>{this.props.title}</h2>
